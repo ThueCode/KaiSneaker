e37692b66274624d58290d7a4f8a4b17dd8c829a
アンNè <73763455+antlt1@users.noreply.github.com>
2025-04-14 15:06:31 +0700

    create MODEL API
    - BRANDS
    - PRODUCT

---
+++ BE/APIKaiSneaker/src/models/brand.model.js
@@ -0,0 +1,26 @@
+const db = require('../config/db.config');
+
+const Brand = {
+    getAll: async () => {
+        const [rows] = await db.query('SELECT * FROM brand');
+        return rows;
+    },
+    getById: async (id) => {
+        const [rows] = await db.query('SELECT * FROM BRAND WHERE IDBRAND = ?', [id]);
+        return rows[0];
+    },
+    create: async (newBrand) => {
+        const [result] = await db.query('INSERT INTO BRAND SET ?', [newBrand]);
+        return result.insertId;
+    },
+    update: async (id, updateBrand) => {
+        const [result] = await db.query('UPDATE BRAND SET ? WHERE IDBRAND ?', [updateBrand], id);
+        return result.affectedRows > 0;
+    },
+    delete: async (id) => {
+        const [result] = await db.query('DELETE FROM BRAND WHERE IDBRAND ?');
+        return result.affectedRows > 0;
+    },
+};
+
+module.exports = Brand;
---
+++ BE/APIKaiSneaker/src/models/product.model.js
@@ -0,0 +1,24 @@
+const db = require('../config/db.config');
+
+const Product = {
+    getAll: async () => {
+        const [rows] = await db.query('SELECT * FROM PRODUCT');
+        return rows;
+    },
+    getById: async (id) => {
+        const [rows] = await db.query('SELECT * FROM PRODUCT WHERE SHOESID = ?', id);
+        return rows[0];
+    },
+    create: async (newProduct) => {
+        const [result] = await db.query('INSERT INTO PRODUCT SET ?', [newProduct]);
+        return result.insertId;
+    },
+    update: async (id, updateProduct) => {
+        const [result] = await db.query('UPDATE PRODUCT SET ? WHERE SHOESID = ?', [updateProduct], id);
+        return result.affectedRows > 0;
+    },
+    delete: async (id) => {
+        const [result] = await db.query('DELETE FROM PRODUCT WHERE SHOESID = ?', id);
+        return result.affectedRows > 0;
+    },
+};
